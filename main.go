65b6652dc1af3f9859afd6404501cc5c3b0cd152
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-02-19 08:26:24 +0700

    salah njir hahahahahah

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import "fmt"
 
-func HowMuchILoveYou(i int) string {
-	arr := []string{"I love you", "a little", "a lot", "passionately", "madly", "not at all"}
-	
-	if i > len(arr) {
-		return arr[(i - 1) % len(arr)]
-	} else {
-		return arr[i - 1]
-	}
-}
 
 func main() {
-	fmt.Println(HowMuchILoveYou(255))
+
 }
--- soal/HowMuchILoveYou.go
+++ soal/HowMuchILoveYou.go
@@ -16,3 +16,13 @@
 
 // Answer :
 package soal
+
+func HowMuchILoveYou(i int) string {
+	arr := []string{"I love you", "a little", "a lot", "passionately", "madly", "not at all"}
+	
+	if i > len(arr) {
+		return arr[(i - 1) % len(arr)]
+	} else {
+		return arr[i - 1]
+	}
+}
