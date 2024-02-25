4181e35c796ed3cf7d2b150b8c3f6a49a528e90f
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-02-25 09:53:30 +0700

    SUSAH NYA

---
+++ soal/FindOdd.go
@@ -0,0 +1,35 @@
+// Question :
+
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+// Answer :
+package soal
+
+import "fmt"
+func FindOdd(seq []int) int {
+	
+	temp := map[int]int{}
+
+	for _, s := range seq {
+		temp[s]++
+	}
+
+	fmt.Println(temp)
+
+	for num, isi := range temp{
+		if isi % 2 != 0{
+			return num
+		}
+	}
+
+
+	return seq[0]
+
+}
