e483d7c6c7ade149b292caa54d8a8333119dd2fb
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-03-10 18:09:09 +0700

    huffft

---
+++ soal/FindMultiples.go
@@ -0,0 +1,18 @@
+// Question :
+
+// In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+// For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+// Answer :
+package soal
+
+func FindMultiples(integer, limit int) (res[]int) {
+
+	temp := integer
+
+	for integer <= limit {
+		res = append(res, integer)
+		integer += temp
+	}
+	return 
+  }
