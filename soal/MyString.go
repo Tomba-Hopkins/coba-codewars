6a1b8927ed0610336a392d95b06fc44756f5666b
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-03-13 08:21:36 +0700

    sbebsbebbsbe

---
+++ soal/MyString.go
@@ -0,0 +1,37 @@
+// Question :
+
+// Is the string uppercase?
+// Task
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+// Answer :
+package soal
+
+import "strings"
+
+type MyString string
+
+func (s MyString) IsUpperCase() bool {
+	temp := 0
+
+	for i := 0; i < len(s); i++ {
+		if string(s[i]) != strings.ToUpper(string(s[i])){
+			temp++
+		}
+	}
+
+	if temp > 0 {
+		return false
+	}
+
+	return true
+}
