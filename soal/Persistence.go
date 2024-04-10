fdff2590351737777432f5caf93e3ba9a1768433
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-04-11 06:10:24 +0700

    gratitude.

---
+++ soal/Persistence.go
@@ -0,0 +1,34 @@
+// Question :
+
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// For example (Input --> Output):
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+// Answer :
+package soal
+
+import "strconv"
+
+func Persistence(n int) (res int) {
+
+	strN := strconv.Itoa(n)
+
+	for len(strN) > 1 {
+		temp := 1
+		for i := 0; i < len(strN); i++{
+			num, _ := strconv.Atoi(string(strN[i]))
+			temp *= num
+		}
+
+		strTemp := strconv.Itoa(temp)
+		strN = strTemp
+		res++
+		
+	}
+
+	return
+
+	
+}
