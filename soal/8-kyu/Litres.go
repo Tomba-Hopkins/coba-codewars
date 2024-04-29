1384edb60efd2ba4da005ba7a51ca41976d32a54
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-04-29 09:32:47 +0700

    hufffttft

---
+++ soal/8-kyu/Litres.go
@@ -0,0 +1,21 @@
+// Question :
+
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+// For example:
+// time = 3 ----> litres = 1
+// time = 6.7---> litres = 3
+// time = 11.8--> litres = 5
+
+// Answer :
+package soal
+
+import "math"
+
+func Litres(time float64) int {
+
+	segelas := math.Floor(time * 0.5)	
+	return int(segelas)
+
+}
