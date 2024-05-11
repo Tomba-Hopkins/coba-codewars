e28c48d411f367787542b6d5924ad108885e73d0
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-05-11 19:42:16 +0700

    haccuh

---
+++ soal/8-kyu/Between.go
@@ -0,0 +1,18 @@
+// Question :
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+// For example:
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+// Answer :
+package soal
+
+func Between(a, b int) (res []int) {
+	for i := a; i <= b; i++ {
+		res = append(res, i)
+	}
+
+	return
+}
