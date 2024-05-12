7d3aa669c2843f3a04b72c328b44bc2a12c127e5
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-05-13 06:30:31 +0700

    message: and the rain will kul us all

---
+++ soal/7-kyu/RoundToNext5.go
@@ -0,0 +1,34 @@
+// Question :
+
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+// Examples:
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+// You can assume that all inputs are valid integers.
+
+// Answer :
+package soal
+
+func RoundToNext5(n int) int {
+
+	if n % 5 == 0 {
+		return n
+	}
+
+	n += 5
+
+	for n % 5 != 0 {
+		n--
+	}
+	
+	return n
+}
